418758e341f4493109017b59083045624690f6ba
EZ-Swipe <den5790kuba@gmail.com>
2024-03-13 20:48:58 +0200

    socket completed

--- backend/server.js
+++ backend/server.js
@@ -6,17 +6,15 @@ import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
 import userRoutes from "./routes/user.routes.js";
 
-
 import connectToMongoDB from "./db/connectToMongoDB.js";
+import { app, server } from "./socket/socket.js";
 
-const app = express();
 const PORT = process.env.PORT || 5000;
 
 dotenv.config();
 
 app.use(express.json());
 app.use(cookieParser());
-
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
@@ -27,7 +25,7 @@ app.use("/api/users", userRoutes);
 //   res.send("Hello World!");
 // });
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   connectToMongoDB();
   console.log(`Server Running on Port ${PORT}`);
 });
